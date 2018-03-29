f6bb391f5085faabe0fedbac26e917af7b3f28db
Gregory Sward <Greg@Gregorys-MacBook-Pro.local>
2018-03-28 21:54:26 -0400

    removed patterns from urls

--- cms_shiny/urls.py
+++ cms_shiny/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from cms_shiny.views import ShinyAppListView, ShinyAppDetailView
 
-urlpatterns = patterns('',
-    url(r'^$', ShinyAppListView.as_view(), name='shiny_list'),
-    url(r'^(?P<slug>[^/]+)/$', ShinyAppDetailView.as_view(), name='shiny_detail'),
-)
+urlpatterns = [
+               url(r'^$', ShinyAppListView.as_view(), name='shiny_list'),
+               url(r'^(?P<slug>[^/]+)/$', ShinyAppDetailView.as_view(), name='shiny_detail'),
+               ]
